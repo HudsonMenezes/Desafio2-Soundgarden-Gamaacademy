053d768552cde9d71eaf165f2d71b254ee7879d3
Hudson Menezes <hudson.bm23@gmail.com>
2022-09-18 16:37:29 -0300

    criar evento js criado

---
+++ js/criarevento.js
@@ -0,0 +1,4 @@
+const criarEventoForm = document.querySelector('#criarEventoForm')
+criarEventoForm.addEventListener('submit', novoEvento)
+
+const URL = 'https://xp41-soundgarden-api.herokuapp.com/events'
