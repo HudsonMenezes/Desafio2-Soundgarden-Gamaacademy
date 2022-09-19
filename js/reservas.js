d2cca48d51963179516a473537c43c7c43237618
Hudson Menezes <hudson.bm23@gmail.com>
2022-09-19 16:33:15 -0300

    reservas alteracoes

--- js/reservas.js
+++ js/reservas.js
@@ -1,32 +1,39 @@
-// lista reservas de um evento
+// função para resgatar as reservas através do parametro ID da URL no browser e mostrar no painel
 
-function getReservas() {
+async function muralReservas() {
   const queryString = window.location.search
   const urlParams = new URLSearchParams(queryString)
   const id = urlParams.get('id')
-  const url = 'https://xp41-soundgarden-api.herokuapp.com/bookings/event/' + id
-  fetch(url)
-    .then(response => response.json())
-    .then(data => listarReservas(data))
-    .catch(err => console.log(err))
+  const URL = 'https://xp41-soundgarden-api.herokuapp.com/bookings/event/' + id
+
+  try {
+    const response = await fetch(URL)
+    const data = await response.json()
+    listaDasReservas(data)
+  } catch (error) {
+    console.log(error)
+  }
 }
 
-function listarReservas(reservas) {
-  const titulo = reservas[0].event.name
+function listaDasReservas(reservas) {
+  const tituloReserva = reservas[0].event.name
   document.getElementById(
     'eventoNome'
-  ).innerHTML = `Reservas referentes ao evento ${titulo.bold()}`
+  ).innerHTML = `Reservas referentes ao evento <br>${tituloReserva.bold()}`
   reservas.forEach((reserva, index) => {
-    const tr = document.createElement('tr')
-    tr.innerHTML = `<th scope="row">${index + 1}</th>
-        <td>${reserva.owner_name}</td>
-        <td>${reserva.owner_email}</td>
-        <td>${reserva.number_tickets}</td>`
+    const linha = document.createElement('tr')
+    linha.innerHTML = `
+    <th scope="row">${index + 1}</th>
+      <td>${reserva.owner_name}</td>
+      <td>${reserva.owner_email}</td>
+      <td>${reserva.number_tickets}</td>
+      `
 
-    document.querySelector('table tbody').appendChild(tr)
+    // adicionando o html "linha" como filho da tag tbody
+    document.querySelector('tbody').appendChild(linha)
   })
 }
 
 // chamando a function
 
-getReservas()
+muralReservas()
