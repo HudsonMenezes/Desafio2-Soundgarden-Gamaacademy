e907c2a2a99d59e362a383baaf40e0b3caab9664
Hudson Menezes <hudson.bm23@gmail.com>
2022-09-21 00:03:51 -0300

    updates

--- js/banner.js
+++ js/banner.js
@@ -1,6 +1,6 @@
 var imgArray = ['img/banner1.png', 'img/banner2.png', 'img/banner3.png']
 var curIndex = 0
-var imgDuration = 5000
+var imgDuration = 3000
 
 function slideShow() {
   document.getElementById('banner').src = imgArray[curIndex]
