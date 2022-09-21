fddab561e0fe7786b03e60b7cc7c21b8a83f1746
Hudson Menezes <hudson.bm23@gmail.com>
2022-09-20 23:59:58 -0300

    adicionado banner no index.html

---
+++ js/banner.js
@@ -0,0 +1,13 @@
+var imgArray = ['img/banner1.png', 'img/banner2.png', 'img/banner3.png']
+var curIndex = 0
+var imgDuration = 5000
+
+function slideShow() {
+  document.getElementById('banner').src = imgArray[curIndex]
+  curIndex++
+  if (curIndex == imgArray.length) {
+    curIndex = 0
+  }
+  setTimeout('slideShow()', imgDuration)
+}
+slideShow()
